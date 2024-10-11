085db166a43ecd02d78bde8970a956ff753cadae
Sewer56 <sewer56lol@googlemail.com>
2024-10-11 15:27:54 +0100

    Added: CommonOffsetPathIndexTuple for presets

--- src/headers/raw/toc/v2/presets/mod.rs
+++ src/headers/raw/toc/v2/presets/mod.rs
@@ -1,9 +1,12 @@
+// Tuple in FileEntry struct for first 3 presets.
+pub mod offset_index_path_tuple_p0_p1_p2;
 pub mod preset0_header;
 pub mod preset1_header;
 pub mod preset2_header;
 pub mod preset3_header;
 
 // Prelude
+pub use offset_index_path_tuple_p0_p1_p2::*;
 pub use preset0_header::*;
 pub use preset1_header::*;
 pub use preset2_header::*;
---
+++ src/headers/raw/toc/v2/presets/offset_index_path_tuple_p0_p1_p2.rs
@@ -0,0 +1,162 @@
+use crate::headers::managed::FileEntry;
+use bitfield::bitfield;
+
+bitfield! {
+    /// A tuple consisting of:
+    /// - `u24` DecompressedBlockOffset
+    /// - `u18` FilePathIndex
+    /// - `u22` FirstBlockIndex
+    #[derive(Clone, Copy, PartialEq, Eq, Hash, Default)]
+    pub struct CommonOffsetPathIndexTuple(u64);
+    impl Debug;
+
+    /// `u24` Offset of the file inside the decompressed block.
+    pub u32, decompressed_block_offset, set_decompressed_block_offset: 63, 40;
+
+    /// `u18` Index of the file path associated with this file in the StringPool.
+    pub u32, file_path_index, set_file_path_index: 39, 22;
+
+    /// `u22` Index of the first block associated with this file.
+    pub u32, first_block_index, set_first_block_index: 21, 0;
+}
+
+impl CommonOffsetPathIndexTuple {
+    /// Method for fast initialization of the tuple.
+    ///
+    /// # Arguments
+    ///
+    /// * `decompressed_block_offset` - `u24` Offset of decompressed block.
+    /// * `file_path_index` - `u18` Index of file path in string pool.
+    /// * `first_block_index` - `u22` Index of first block associated with this file.
+    pub fn new(
+        decompressed_block_offset: u32,
+        file_path_index: u32,
+        first_block_index: u32,
+    ) -> Self {
+        let mut tuple = CommonOffsetPathIndexTuple(0);
+        tuple.set_decompressed_block_offset(decompressed_block_offset);
+        tuple.set_file_path_index(file_path_index);
+        tuple.set_first_block_index(first_block_index);
+        tuple
+    }
+
+    /// Method for fast initialization of the tuple from raw data.
+    ///
+    /// # Arguments
+    ///
+    /// * `data` - Raw packed data.
+    pub fn from_raw(data: u64) -> Self {
+        CommonOffsetPathIndexTuple(data)
+    }
+
+    /// Converts the tuple to its raw representation.
+    pub fn into_raw(&self) -> u64 {
+        self.0
+    }
+
+    /// Copy the values of this tuple to a managed [`FileEntry`].
+    #[inline(always)]
+    pub fn copy_to(&self, entry: &mut FileEntry) {
+        entry.decompressed_block_offset = self.decompressed_block_offset();
+        entry.file_path_index = self.file_path_index();
+        entry.first_block_index = self.first_block_index();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::utilities::tests::packing_test_helpers::*;
+    use crate::utilities::tests::permutations::*;
+    use fake::*;
+    use rstest::rstest;
+
+    #[test]
+    fn is_correct_size_bytes() {
+        assert_eq!(size_of::<CommonOffsetPathIndexTuple>(), 8);
+    }
+
+    #[rstest]
+    fn can_be_packed_values_dont_overlap() {
+        let mut tuple = CommonOffsetPathIndexTuple::default();
+        for block_offset in get_bit_packing_overlap_test_values(24) {
+            for path_index in get_bit_packing_overlap_test_values(18) {
+                for block_index in get_bit_packing_overlap_test_values(22) {
+                    // Test all 3 possibilities for overlaps
+                    // A & C
+                    test_packed_properties(
+                        &mut tuple,
+                        |t, v| t.set_decompressed_block_offset(v as u32),
+                        |t| t.decompressed_block_offset() as u64,
+                        |t, v| t.set_first_block_index(v as u32),
+                        |t| t.first_block_index() as u64,
+                        block_offset,
+                        block_index,
+                    );
+
+                    // A & B
+                    test_packed_properties(
+                        &mut tuple,
+                        |t, v| t.set_decompressed_block_offset(v as u32),
+                        |t| t.decompressed_block_offset() as u64,
+                        |t, v| t.set_file_path_index(v as u32),
+                        |t| t.file_path_index() as u64,
+                        block_offset,
+                        path_index,
+                    );
+
+                    // B & C
+                    test_packed_properties(
+                        &mut tuple,
+                        |t, v| t.set_file_path_index(v as u32),
+                        |t| t.file_path_index() as u64,
+                        |t, v| t.set_first_block_index(v as u32),
+                        |t| t.first_block_index() as u64,
+                        path_index,
+                        block_index,
+                    );
+                }
+            }
+        }
+    }
+
+    #[rstest]
+    fn decompressed_block_offset_should_be_24_bits() {
+        assert_size_bits(
+            &mut CommonOffsetPathIndexTuple::default(),
+            |t, v| t.set_decompressed_block_offset(v as u32),
+            |t| t.decompressed_block_offset() as u64,
+            24,
+        );
+    }
+
+    #[rstest]
+    fn file_path_index_should_be_18_bits() {
+        assert_size_bits(
+            &mut CommonOffsetPathIndexTuple::default(),
+            |t, v| t.set_file_path_index(v as u32),
+            |t| t.file_path_index() as u64,
+            18,
+        );
+    }
+
+    #[rstest]
+    fn first_block_index_should_be_22_bits() {
+        assert_size_bits(
+            &mut CommonOffsetPathIndexTuple::default(),
+            |t, v| t.set_first_block_index(v as u32),
+            |t| t.first_block_index() as u64,
+            22,
+        );
+    }
+
+    impl Dummy<Faker> for CommonOffsetPathIndexTuple {
+        fn dummy_with_rng<R: Rng + ?Sized>(_: &Faker, rng: &mut R) -> Self {
+            let mut tuple = CommonOffsetPathIndexTuple::default();
+            tuple.set_decompressed_block_offset(rng.gen_range(0..0xFFFFFF)); // 24 bits
+            tuple.set_file_path_index(rng.gen_range(0..0x3FFFF)); // 18 bits
+            tuple.set_first_block_index(rng.gen_range(0..0x3FFFFF)); // 22 bits
+            tuple
+        }
+    }
+}
